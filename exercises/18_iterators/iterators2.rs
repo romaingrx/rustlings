7d8adfa9cee757d71319d81127b9637e39ac4074
Romain Graux <48758915+romaingrx@users.noreply.github.com>
2024-05-01 14:34:20 +0300

    Add iterators chapter

--- exercises/18_iterators/iterators2.rs
+++ exercises/18_iterators/iterators2.rs
@@ -6,17 +6,19 @@
 // Execute `rustlings hint iterators2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+use std::fmt::format;
 
 // Step 1.
 // Complete the `capitalize_first` function.
 // "hello" -> "Hello"
 pub fn capitalize_first(input: &str) -> String {
-    let mut c = input.chars();
-    match c.next() {
+    let mut chars = input.chars();
+    let mut cap_char = match chars.next() {
         None => String::new(),
-        Some(first) => ???,
-    }
+        Some(first) => String::from(first).to_ascii_uppercase(),
+    };
+    cap_char.push_str(chars.as_str()); // Apparently in-place mut
+    cap_char
 }
 
 // Step 2.
@@ -24,7 +26,7 @@ pub fn capitalize_first(input: &str) -> String {
 // Return a vector of strings.
 // ["hello", "world"] -> ["Hello", "World"]
 pub fn capitalize_words_vector(words: &[&str]) -> Vec<String> {
-    vec![]
+    Vec::from_iter(words.into_iter().map(|w| capitalize_first(w)))
 }
 
 // Step 3.
@@ -32,7 +34,7 @@ pub fn capitalize_words_vector(words: &[&str]) -> Vec<String> {
 // Return a single string.
 // ["hello", " ", "world"] -> "Hello World"
 pub fn capitalize_words_string(words: &[&str]) -> String {
-    String::new()
+    capitalize_words_vector(words).join("")
 }
 
 #[cfg(test)]
